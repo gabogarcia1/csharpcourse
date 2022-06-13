0e30f6f6395cba50fb10cc83d76f233e30843a1e
Gabo <gabegarcia916@gmail.com>
2022-06-13 20:35:22 -0300

    add titles to exercises

--- Promedio/ejercicio7/Program.cs
+++ Promedio/ejercicio7/Program.cs
@@ -1,4 +1,18 @@
-﻿//Pido 10 numeros al usuario
+﻿/*Escribe un programa que:
+
+1) Pida 10 números al usuario.
+2) Obtener la suma de todos los números ingresados.
+3) Obtener cual es el mayor de todos los números.
+4) Obtener cual es el menor de todos los números.
+5) Obtener el promedio de todos los números.
+6) Mostrar todos los números ingresados por pantalla.
+7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
+
+Recuerden que no nos deben adjuntar archivos comprimidos ni imagenes.
+Pueden adjuntarnos el archivo .cs, o el codigo como texto.*/
+
+
+//Pido 10 numeros al usuario
 int[] vector = new int[10];
 for (int i = 0; i < 10; i++)
 {
--- Vectores/ejercicioclase8/Program.cs
+++ Vectores/ejercicioclase8/Program.cs
@@ -1,4 +1,14 @@
-﻿//Primero genero la matriz y le doy valores
+﻿/*Crear un programa que cumpla con los siguientes pasos
+
+1) Crear una matriz de dos dimensiones de tipo int llamada numeros,
+2) Determinar el tamaño de cada dimansion (fila, columnas) mediante valores ingresados por pantalla
+3) Declarar un vector de tipo double llamado promedios
+4) Recorrer la matriz para su carga con elementos de tipo int
+5) Recorrer la matriz para mostrar cada valor de la matriz
+6) Calcular el promedio de cada columna y asignarlo a la posicion correspondiente dentro del vector promedios
+7) Mostrar los promedios recorriendo el vector promedios*/
+
+//Primero genero la matriz y le doy valores
 Console.WriteLine("Elegi la cantidad de filas : ");
 int filas= int.Parse(Console.ReadLine());
 Console.WriteLine("Elegi la cantidad de columnas : ");
