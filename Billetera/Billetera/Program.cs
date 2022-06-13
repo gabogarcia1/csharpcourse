2a59cddd58296481d82e926af4c874e8c7f12eff
Gabo <gabegarcia916@gmail.com>
2022-06-12 21:51:04 -0300

    Tarea terminada

--- Billetera/Billetera/Program.cs
+++ Billetera/Billetera/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿//Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+//BilletesDe10
+//BilletesDe20
+//BilletesDe50
+//BilletesDe100
+//BilletesDe200
+//BilletesDe500
+//BilletesDe1000
+
+//Agregar un método que se llame Total del tipo decimal, y que devuelva el Importe Total en la billetera teniendo en cuenta la cantidad de billetes que se tenga de cada tipo.
+
+//Agregar un metodo que se llame Combinar, que reciba como parámetro otra billetera y que devuelva una nueva Billetera con la suma o combinacion del dinero de ambas billeteras.  
+//Una vez combinadas las 2 billeteras (y creada la tercera), las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+using Billeteras;
+
+Billetera b1 = new Billetera()
+{
+    BilletesDe10 = 1,
+    BilletesDe20 = 1,
+    BilletesDe100 = 1,
+    BilletesDe500 = 1,
+    BilletesDe50 = 1,
+    BilletesDe1000 = 1,
+
+};
+
+Billetera b2 = new Billetera()
+{
+    BilletesDe10 = 1,
+    BilletesDe20 = 1,
+    BilletesDe100 = 1,
+    BilletesDe500 = 1,
+    BilletesDe50 = 1,
+    BilletesDe1000 = 1,
+
+};
+
+Console.WriteLine("El total de billetera 1 es "+ b1.Total());
+Console.WriteLine("El total de la billetera 2 es: " + b2.Total());
+
+Billetera b3 = new Billetera().Combinar(b1, b2);
+
+Console.WriteLine("El ttoal de la nueva billetera es: " + b3.Total());
+Console.WriteLine("El total de la billetera 1 es: " + b1.Total());
+Console.WriteLine("El total de la billetera 2 es: " + b2.Total());
+
+
---
+++ Billetera/Billetera/billetera.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billeteras
+{
+    public class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+
+        public decimal Total()
+        {
+            decimal total = BilletesDe10*10 + BilletesDe100*100 + BilletesDe20*20 + BilletesDe50*50 + BilletesDe500*500 + BilletesDe1000*1000;
+            return total;
+        }
+
+        public Billetera Combinar(Billetera b1, Billetera b2)
+        {
+            Billetera b3 = new Billetera()
+            {
+                BilletesDe10 = b1.BilletesDe10 + b2.BilletesDe10,
+                BilletesDe20 = b1.BilletesDe20 + b2.BilletesDe20,
+                BilletesDe100 = b1.BilletesDe100 + b2.BilletesDe100,
+                BilletesDe500 = b1.BilletesDe500 + b2.BilletesDe500,
+                BilletesDe50 = b1.BilletesDe50 + b2.BilletesDe50,
+                BilletesDe1000 = b1.BilletesDe1000 + b2.BilletesDe1000,
+            };
+
+            b1.BilletesDe10 = 0;
+            b1.BilletesDe20 = 0;
+            b1.BilletesDe50 = 0;
+            b1.BilletesDe100 = 0;
+            b1.BilletesDe500 = 0;
+            b1.BilletesDe1000 = 0;
+
+
+
+            b2.BilletesDe10 = 0;
+            b2.BilletesDe20 = 0;
+            b2.BilletesDe50 = 0;
+            b2.BilletesDe100 = 0;
+            b2.BilletesDe500 = 0;
+            b2.BilletesDe1000 = 0;
+
+
+
+            return b3;
+        }
+
+
+    }
+}
